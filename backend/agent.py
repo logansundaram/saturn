966d8a2e38ceb231521fe3cce5ee2ad6d4ffc7ea
Logan Sundaram <logantechwork@gmail.com>
2026-05-17 13:31:12 -0700

    conditional edges for tool calling implemneted correctly

--- backend/agent.py
+++ backend/agent.py
@@ -4,13 +4,20 @@ from langchain.tools import tool
 from langchain.messages import SystemMessage, HumanMessage, ToolMessage
 from typing import List, Dict, Any, Optional
 
-call_tool_msg = SystemMessage(content="Call the relevante tools based on the user request")
+call_tool_msg = SystemMessage(
+    content="Call the relevante tools based on the user request"
+)
 
-fetch_docs_msg = SystemMessage(content="Fetch the relevant documents based on the user request") 
+fetch_docs_msg = SystemMessage(
+    content="Fetch the relevant documents based on the user request"
+)
 
-synthesize_output_msg = SystemMessage(content="Synthesize the output based on the user request")
+synthesize_output_msg = SystemMessage(
+    content="Synthesize the output based on the user request"
+)
 
-llm = ChatOllama(model="qwen3.6:35b")
+llm = ChatOllama(model="gemma4:e4b")
+
 
 @tool
 def addition(a: int, b: int) -> int:
@@ -18,20 +25,22 @@ def addition(a: int, b: int) -> int:
     return a + b
 
 
-
 def fetch_docs(state: MessagesState):
     relevant_docs = "my name is logan"
     return {"messages": relevant_docs}
 
+
 def call_tools(state: MessagesState):
-    #double check this is correct syntax for appending system_message
+    # double check this is correct syntax for appending system_message
     llm_response = llm_with_tools.invoke(state["messages"] + [call_tool_msg])
-    return {"messages" : llm_response}
+    return {"messages": llm_response}
+
 
 tools = [addition]
 tools_by_name = {t.name: t for t in tools}
 
 llm_with_tools = llm.bind_tools(tools)
+
 
 def tool_node(state: MessagesState):
     result = []
@@ -41,36 +50,41 @@ def tool_node(state: MessagesState):
         observation = selected_tool.invoke(tool_call["args"])
 
         result.append(
-            ToolMessage(
-                content=str(observation),
-                tool_call_id=tool_call["id"]
-            )
+            ToolMessage(content=str(observation), tool_call_id=tool_call["id"])
         )
 
     return {"messages": result}
 
+
 def synthesize_output(state: MessagesState):
     llm_response = llm.invoke(state["messages"] + [synthesize_output_msg])
-    return {"messages" : llm_response}
+    return {"messages": llm_response}
 
+
+def tools_necessary(state: MessagesState):
+    if state["messages"][-1].tool_calls:
+        return True
+    return False
 
 
 builder = StateGraph(MessagesState)
-#langgraph nodes
+# langgraph nodes
 builder.add_node("fetch_docs", fetch_docs)
 builder.add_node("call_tools", call_tools)
 builder.add_node("tool_node", tool_node)
 builder.add_node("synthesize_output", synthesize_output)
 
-#build out graph edges
-#should be fetch docs -> call tools -> execute tools -> synthesize output
-#basic proof of concept agent
+# build out graph edges
+# should be fetch docs -> call tools -> execute tools -> synthesize output
+# basic proof of concept agent
 builder.add_edge(START, "fetch_docs")
 builder.add_edge("fetch_docs", "call_tools")
-builder.add_edge("call_tools", "tool_node")
+# condiitonal edge if tools calls are needed
+builder.add_conditional_edges(
+    "call_tools", tools_necessary, {True: "tool_node", False: "synthesize_output"}
+)
 builder.add_edge("tool_node", "synthesize_output")
 builder.add_edge("synthesize_output", END)
-
 
 graph = builder.compile()
 

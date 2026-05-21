af233c948d4b2986710eab773b2c20df26dcf03d
Logan Sundaram <logantechwork@gmail.com>
2026-05-21 01:27:02 -0700

    started on light and moderate subgraphs to be router to

--- backend/workflows/light.py
+++ backend/workflows/light.py
@@ -6,12 +6,13 @@ from llms import llm_with_tools
 from messages import light_llm_msg
 
 
-def llm_call(state: MessagesState):
-    return llm_with_tools.invoke(state["messages"] + [light_llm_msg])
+def build_light():
+    def llm_call(state: MessagesState):
+        return llm_with_tools.invoke(state["messages"] + [light_llm_msg])
 
-
-light_builder = StateGraph(MessagesState)
-light_builder.add_node("llm_call", llm_call)
-light_builder.add_edge(START, "llm_call")
-light_builder.add_edge("llm_call", END)
-light_graph = light_builder.compile()
+    light_builder = StateGraph(MessagesState)
+    light_builder.add_node("llm_call", llm_call)
+    light_builder.add_edge(START, "llm_call")
+    light_builder.add_edge("llm_call", END)
+    light_graph = light_builder.compile()
+    return light_graph
--- backend/workflows/moderate.py
+++ backend/workflows/moderate.py
@@ -1,1 +1,24 @@
 # build and compile moderate subgraph to be routed to from main graph
+from langgraph.graph import StateGraph, START, END, MessagesState
+
+from llms import llm_with_tools, llms
+
+from messages import light_llm_msg
+
+
+def build_moderate():
+    def llm_call(state: MessagesState):
+        return llm_with_tools.invoke(state["messages"] + [light_llm_msg])
+
+    light_builder = StateGraph(MessagesState)
+    light_builder.add_node("llm_call", llm_call)
+    light_builder.add_edge(START, "llm_call")
+    light_builder.add_edge("llm_call", END)
+    light_graph = light_builder.compile()
+
+    moderate_builder = StateGraph(MessagesState)
+    moderate_builder.add_node("llm_call", llm_call)
+    moderate_builder.add_edge(START, "llm_call")
+    moderate_builder.add_edge("llm_call", END)
+    moderate_graph = moderate_builder.compile()
+    return moderate_graph
